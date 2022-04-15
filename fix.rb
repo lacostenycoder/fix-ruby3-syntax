db42ff7f4550b077391e7972a418deee4dc9312e
Lance Jordan <djlancejordan@gmail.com>
2022-04-15 09:31:31 -0400

    this might actually work 🤞

--- fix.rb
+++ fix.rb
@@ -14,10 +14,10 @@ def update_to_explicit_hash(string)
   matches = string.scan /(?<=\()[^()]+(?=\))/
 
   matches.each do |match_string|
-    stripped = match_string.squish
+    squished = match_string.squish
 
     # Check if hash string and if it isn't prefixed with a bracket.
-    if (stripped.chars.first != '{' and stripped.match? /:|=>/)
+    if (squished.chars.first != '{' and squished.match? /:|=>/)
       new_string = "{#{match_string}}"
       if valid_hash?(new_string.squish)
         string = string.gsub(match_string, new_string)
@@ -28,6 +28,13 @@ def update_to_explicit_hash(string)
   string
 end
 
+def eval_word(word)
+  # if word.include?('user_message')
+  #   binding.pry
+  # end
+  word rescue nil
+end
+
 def valid_hash?(string)
   begin
     #=> "{:key_1=>true,\"key_2\":false}"
@@ -35,14 +42,14 @@ def valid_hash?(string)
     #=> "{\"key_1\":true,\"key_2\":false}"
     string.gsub!(/:(\w+)\s*=>/, '"\1":')
     #strip string interpolation
-    # string.gsub!(/\#\{+(?<=\#\{)[^()]+(?=\}\")+\}/, '')
-    my_hash = JSON.parse(string, {symbolize_names: true})
+    string.gsub!(/\#\{+(?<=\#\{)[^()]+(?=\}\")+\}/, '')
+    # replace undefined vars 
+    hash_string = string.split(/\s\:/).map{|w| eval_word(w)}.join
+    my_hash = JSON.parse(hash_string, {symbolize_names: true})
     #=> {:key_1=>true, :key_2=>false}
     my_hash.is_a? Hash
   rescue JSON::ParserError
-    binding.pry
-    # string.gsub!(/\#\{+(?<=\#\{)[^()]+(?=\}\")+\}/, '')
-    false
+    hash_string.is_a? Hash
   end
   true
 end
